cf312bf95b32535a70267834b775315933d1ae57
ThanhMinh <thanhminh339968az@gmail.com>
2023-09-19 10:02:04 +0700

    Add bai-8

---
+++ bai-7/main.js
@@ -0,0 +1,13 @@
+/*
+Giới thiệu một số hàm built-in
+    1. Alert
+    2. Console
+    3. Confirm
+    4. Prompt
+    5. Set timeout
+    6. Set interval
+*/
+   
+setInterval(function() {
+    console.log('Day la log' + Math.random)
+}, 1000)
