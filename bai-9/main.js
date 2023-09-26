fcee0b829fe1484611e639b52d7aebe05af4bf05
ThanhMinh <thanhminh339968az@gmail.com>
2023-09-26 08:51:41 +0700

    Add bai-9

---
+++ bai-9/main.js
@@ -0,0 +1,17 @@
+/** Toan tu so hoc
++        --> Cong
+-        --> Tru
+*        --> Chia
+**       --> Luy thua
+/        --> Chia
+%        --> Chia lay so du
+++       --> Tang 1 gia tri so
+--       --> Giam 1 gia tri so
+ */
+
+var a = 9;
+a++;
+a--;
+a++;
+
+console.log(a);
