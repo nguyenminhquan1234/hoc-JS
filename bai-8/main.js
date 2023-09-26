1538865620f130de6c1de49afa81bbe3150821fa
ThanhMinh <thanhminh339968az@gmail.com>
2023-09-26 08:36:12 +0700

    Add bai-8

---
+++ bai-8/main.js
@@ -0,0 +1,16 @@
+/**
+
+Giới thiệu về toán tử trong Javascript
+1. Toán tử số học - Arithmetic
+2. Toán tử gán - Assignment
+3. Toán tử so sánh - Comparion
+4. Toán tử logic - Logical
+
+ */
+
+var a = 1;
+var b = 2;
+
+if (a > 0 && b < 0) {
+    alert('a & b lớn hơn 0')
+}
